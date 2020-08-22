4b12ffe3791707a9e4c8d3f724329b920aa382c5
Debanu Gupta <debanu.gupta@in.ibm.com>
2020-08-22 20:18:18 +0530

    End of section 11

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -3,7 +3,7 @@ using AutoMapper;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 
-namespace DatingApp.API.Helper
+namespace DatingApp.API.Helpers
 {
     public class AutoMapperProfiles : Profile
     {
@@ -21,6 +21,8 @@ namespace DatingApp.API.Helper
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,7 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
 
-namespace DatingApp.API.Helper
+namespace DatingApp.API.Helpers
 {
     public static class Extensions
     {
